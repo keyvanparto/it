4fb8d115251d957ed21279b79fd4553c013e8dbc
berserkey <107687295+berserkey@users.noreply.github.com>
2024-11-21 00:33:16 +0100

    second and third post

---
+++ assets/js/data/mathjax.js
@@ -0,0 +1,16 @@
+MathJax = {
+  tex: {inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]']
+    ],tags: 'ams'
+  }
+};
+
+window.MathJax = {
+  tex: {
+    tags: 'none', // Disabilita la numerazione automatica
+  },
+};
